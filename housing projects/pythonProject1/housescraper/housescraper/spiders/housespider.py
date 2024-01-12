ebb707c1819f2d313dfc9864275ea6a61ed7cfa6
AltafWilkens <66486547+AltafWilkens@users.noreply.github.com>
2024-01-12 22:14:37 +0200

    Add files via upload

--- housing projects/pythonProject1/housescraper/housescraper/spiders/housespider.py
+++ housing projects/pythonProject1/housescraper/housescraper/spiders/housespider.py
@@ -7,27 +7,22 @@ class HousespiderSpider(scrapy.Spider):
     start_urls = ["https://www.property24.com/apartments-for-sale/gauteng/1"]
 
     def parse(self, response):
-        houses = response.css('.p24_regularTile')
+        houses = response.css('.p24_content')
+
         for house in houses:
             yield {
-                'floor_size': house.css('.p24_icons.p24_size span::text').get()
+                'price' : house.css('.p24_price::attr(content)').get(),
+                'discription' : house.css('.p24_title ::text').get(),
+                'location' : house.css('.p24_location::text').get(),
+                'featureDetails ': house.css('.p24_icons .p24_featureDetails span::text').getall(),
+                'floor_size': house.css('.p24_icons .p24_size span::text').get(),
             }
-            url = house.css('.p24_regularTile a::(href)').get()
 
-            house_url = 'https://www.property24.com/' + url
-            yield response.follow(house_url, callback=self.parse_house_page)
-            #'price' : house.css('.p24_information .p24_price::text').get(),
-            #'discription' : house.css('.p24_information .p24_description::text').get(),
-            #'location' : house.css('.p24_information .p24_description .p24_location::text').get(),
-            #'featureDetails ' : house.css('.p24_icons .p24_featureDetails span::text').getall(),
-            #',
+        next_page = response.css('a.pull-right::attr(href)').get()
 
-
-        #next_page = response.css('a.pull-right::attr(href)').get()
-
-        #if next_page is not None:
-            #next_page_url = next_page
-            #yield response.follow(next_page_url, callback=self.parse)
+        if next_page is not None:
+            next_page_url = next_page
+            yield response.follow(next_page_url, callback=self.parse)
 
 
     def parse_house_page(self, response):
